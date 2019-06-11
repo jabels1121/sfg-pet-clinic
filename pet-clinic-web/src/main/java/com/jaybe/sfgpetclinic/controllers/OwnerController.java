1e475241a9989b04933b99ac7743655d1a9ab405
Гришаев Илья Андреевич <i.grishaev@csat.ru>
2019-06-11 08:05:11 +0300

    Adding Owner index page and controller. Closes #13.

--- pet-clinic-web/src/main/java/com/jaybe/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/jaybe/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,16 @@
 package com.jaybe.sfgpetclinic.controllers;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/vets")
-public class VetController {
+@RequestMapping("/owners")
+public class OwnerController {
 
     @RequestMapping(path = {"", "/", "/index", "/index.html"})
-    public String listVets() {
-        return "vets/index";
+    public String listOfOwners(){
+        return "owners/index";
     }
+
 }
