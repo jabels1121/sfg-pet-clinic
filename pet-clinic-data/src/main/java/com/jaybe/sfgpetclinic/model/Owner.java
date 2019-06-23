dd91b2ee3204057b2c8fadd0d58e18f0bce3da9a
Jabels <jabels1121@gmail.com>
2019-06-23 20:17:09 +0300

    Added visit, updated owner with pet relationship. Closes #26.

--- pet-clinic-data/src/main/java/com/jaybe/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/jaybe/sfgpetclinic/model/Owner.java
@@ -1,5 +1,16 @@
 package com.jaybe.sfgpetclinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Owner extends Person {
+
+    private Set<Pet> pets;
 
 }
---
+++ pet-clinic-data/src/main/java/com/jaybe/sfgpetclinic/model/Visit.java
@@ -0,0 +1,18 @@
+package com.jaybe.sfgpetclinic.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Visit extends BaseEntity{
+
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+}
