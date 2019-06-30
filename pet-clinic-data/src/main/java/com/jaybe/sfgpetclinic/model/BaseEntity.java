67403996aaea85b2e157480e9a15ee44e1acf931
Jabels <jabels1121@gmail.com>
2019-06-30 22:59:45 +0300

    Update baseEntity with JPA annotations

--- pet-clinic-data/src/main/java/com/jaybe/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/jaybe/sfgpetclinic/model/BaseEntity.java
@@ -3,12 +3,19 @@ package com.jaybe.sfgpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
     @Getter
     @Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
 }
--- pet-clinic-web/src/main/java/com/jaybe/sfgpetclinic/restControllers/VetRestController.java
+++ pet-clinic-web/src/main/java/com/jaybe/sfgpetclinic/restControllers/VetRestController.java
@@ -2,12 +2,17 @@ package com.jaybe.sfgpetclinic.restControllers;
 
 import com.jaybe.sfgpetclinic.model.Vet;
 import com.jaybe.sfgpetclinic.services.VetService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Set;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
 
 @RestController
 public class VetRestController extends AbstractRestController {
@@ -23,8 +28,13 @@ public class VetRestController extends AbstractRestController {
         return vetService.findAll();
     }
 
-    @PostMapping(path = {"/vets/create", "/vets/create"})
-    public Vet createVet(@RequestBody Vet vet) {
-        return vetService.save(vet);
+    @PostMapping(
+            path = {"/vets/create", "/vets/create"},
+            consumes = {APPLICATION_JSON_VALUE, APPLICATION_XML_VALUE},
+            produces = {APPLICATION_JSON_VALUE, APPLICATION_XML_VALUE}
+    )
+    public ResponseEntity<Vet> createVet(@RequestBody Vet vet) {
+        Vet save = vetService.save(vet);
+        return new ResponseEntity<>(save, HttpStatus.OK);
     }
 }
