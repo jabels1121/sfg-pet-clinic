1e2b4c61113ba8ea43166f3100b18e8b573237f5
Jabels <jabels1121@gmail.com>
2019-07-01 14:20:58 +0300

    Added sd jpa service for PetType. Closes #42.

---
+++ pet-clinic-data/src/main/java/com/jaybe/sfgpetclinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -0,0 +1,48 @@
+package com.jaybe.sfgpetclinic.services.springdatajpa;
+
+import com.jaybe.sfgpetclinic.model.PetType;
+import com.jaybe.sfgpetclinic.repositories.PetTypeRepository;
+import com.jaybe.sfgpetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeSDJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSDJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return StreamSupport.stream(petTypeRepository.findAll().spliterator(), false)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) throws RuntimeException {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/jaybe/sfgpetclinic/services/springdatajpa/PetsSDJpaService.java
@@ -0,0 +1,48 @@
+package com.jaybe.sfgpetclinic.services.springdatajpa;
+
+import com.jaybe.sfgpetclinic.model.Pet;
+import com.jaybe.sfgpetclinic.repositories.PetRepository;
+import com.jaybe.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+@Profile("springdatajpa")
+public class PetsSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetsSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        return StreamSupport.stream(petRepository.findAll().spliterator(), false)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) throws RuntimeException {
+        return null;
+    }
+
+    @Override
+    public void delete(Pet object) {
+
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+}
